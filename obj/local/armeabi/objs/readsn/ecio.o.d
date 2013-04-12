a5ef0ff4b11621dc8395f6a3efa404ba67f16c38
tody_slax_git <tody@slax.org>
2013-04-12 17:37:17 +0800

    first release to github

---
+++ obj/local/armeabi/objs/readsn/ecio.o.d
@@ -0,0 +1,193 @@
+obj/local/armeabi/objs/readsn/ecio.o: jni/ecio.c \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/string.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/android/api-level.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/jni.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/stdio.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/strings.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/memory.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/unistd.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/select.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/time.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/time.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/signal.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/limits.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/syslimits.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/signal.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/signal.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/siginfo.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/siginfo.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/sysconf.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/capability.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/pathconf.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/fcntl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/fcntl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/fcntl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/fcntl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/wait.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/resource.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/resource.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/resource.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/resource.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/wait.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/errno.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/errno.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/errno.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/errno.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/errno-base.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/android/log.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/ioctl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/ioctl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/ioctl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/ioctl.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/ioctls.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/termbits.h \
+ /media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/ioctl_compat.h
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/string.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/android/api-level.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/stdio.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/unistd.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/select.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/time.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/time.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/signal.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/signal.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/signal.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/siginfo.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/sysconf.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/capability.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/pathconf.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/fcntl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/fcntl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/fcntl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/wait.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/resource.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/resource.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/resource.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/resource.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/wait.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/errno.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/errno.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/errno.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/errno.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/android/log.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/ioctl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/linux/ioctl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/ioctl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/ioctls.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/asm/termbits.h:
+
+/media/fa08f3c7-4e04-4538-85dc-d78cb40b6079/home/tody/android-ndk-r8b/platforms/android-5/arch-arm/usr/include/sys/ioctl_compat.h:
---
+++ src/com/tware/macup/MACUpActivity.java
@@ -0,0 +1,284 @@
+package com.tware.macup;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Process;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MACUpActivity extends Activity {
+    /** Called when the activity is first created. */
+	final public String TAG = "MACUp";
+	private static String RKPATH = "/dev/rknand_sys_storage";
+	private EditText ipAddr;
+	private EditText port;
+	private EditText mac;
+	private EditText sn;
+	private Button upMac;
+	private WifiManager mWifi;
+	private String WifiMac;
+	private TextView ret;
+	private String serialnumber;
+	private Boolean snOk = true;
+	private String TESTLOG;  // 2012-03-05 added
+	private String LOGMSG;   // 2012-03-05 added
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.tware.macup.R.layout.main);
+        
+        ipAddr = (EditText)findViewById(com.tware.macup.R.id.E_address);
+        port	= (EditText)findViewById(com.tware.macup.R.id.E_port);
+        mac	= (EditText)findViewById(com.tware.macup.R.id.E_mac);
+        upMac	= (Button)findViewById(com.tware.macup.R.id.B_macup);
+        ret	= (TextView)findViewById(com.tware.macup.R.id.V_ret);
+        sn = (EditText)findViewById(com.tware.macup.R.id.E_sn);
+
+        /*
+	*  Read Serial number from JNI. 
+	*/
+	if (isRk30())
+	{
+	  log.e(TAG, "Read SN for Rk30");
+	  sn.setText(rkReadSn().toUpperCase());
+	}else{
+	  log.e(TAG, "Read SN for Nvidia or Amlogic");	
+	  sn.setText(ReadSerial().toUpperCase());
+	}
+	
+        serialnumber = sn.getText().toString().trim();
+
+        if (serialnumber.contains("FAIL") || serialnumber.equals(""))
+        {
+        	snOk = false;
+        	sn.setTextColor(Color.RED);
+        	sn.setText("N/A");
+        }
+        
+        serialnumber = sn.getText().toString().trim(); // 2012-03-05 added
+        
+        TESTLOG = this.getIntent().getStringExtra("LOG"); // 2012-03-05 added
+        
+	mWifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+
+        if (!mWifi.isWifiEnabled())
+        {
+        	mWifi.setWifiEnabled(true);
+        }
+        
+        if (serialnumber.length() < 12 && !sn.getText().toString().equals("N/A")) // 2012-03-28 added
+        {
+        	new AlertDialog.Builder(this).setIcon(R.drawable.mac)
+        		.setTitle("错误")
+        		.setMessage("机台序列号位数不正确，请重流前测！")
+        		.setPositiveButton("Ok", null)
+        		.show();
+        	sn.setTextColor(Color.RED);
+        	upMac.setEnabled(false);
+        	return ; 
+        }
+        
+        upMac.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v)
+        	{
+                WifiInfo wifiInfo = mWifi.getConnectionInfo();
+                
+                if((WifiMac = wifiInfo.getMacAddress())== null)
+                {
+                	new AlertDialog.Builder(MACUpActivity.this)
+                		.setTitle("Warning")
+                		.setMessage("No MAC Address Found!")
+                		.setPositiveButton("", null);
+                	return ;
+                }
+                WifiMac = WifiMac.toUpperCase();
+                mac.setText(WifiMac.toUpperCase());
+
+                LOGMSG = "NOSN|" + WifiMac; // 2012-03-05 added
+
+                if (snOk)
+                {
+                	LOGMSG = serialnumber + '|' + WifiMac;
+                }
+                
+                
+                if(TESTLOG != null) // 2012-03-05 added new log format, before info.
+                {
+                	if(snOk)
+                	{
+                		LOGMSG = serialnumber + TESTLOG;
+                	}else{
+                		LOGMSG = "NOSN" + TESTLOG;
+                	}
+                }
+                
+                try {
+                	// 2012-03-05: change sendMac(WifiMac+"\0")) to sendMac(LOGMSG+"\0") 
+                	if(sendMac(LOGMSG+"\0"))  //Sending MAC Address to Server
+					{
+		                ret.setTextColor(Color.GREEN);
+		                upMac.setTextColor(Color.GREEN);
+		                ret.append("\n :o-----> Upload Ok");
+		                upMac.setEnabled(false);						
+					}
+					else
+					{
+		                ret.setTextColor(Color.RED);
+		                upMac.setTextColor(Color.RED);
+		                ret.append("\n :<-----> Upload fail");
+					}
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+	                ret.setTextColor(Color.RED);
+	                upMac.setTextColor(Color.RED);
+	                ret.append("\n :n-----> Upload Fail");
+
+					return ;
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+	                ret.setTextColor(Color.RED);
+	                upMac.setTextColor(Color.RED);
+	                ret.append("\n :u-----> Upload Fail");
+					return ;
+				} catch (IOException e) {
+					e.printStackTrace();
+	                ret.setTextColor(Color.RED);
+	                upMac.setTextColor(Color.RED);
+	                ret.append("\n :i-----> Upload Fail");
+					return ;
+				}
+        	}
+        });
+    }
+    
+    boolean sendMac(String WifiMac) throws NumberFormatException, UnknownHostException, IOException
+    {
+    	Socket sock = new Socket(ipAddr.getText().toString(),
+    							Integer.parseInt(port.getText().toString()));
+    	
+        OutputStream sout = sock.getOutputStream();
+        InputStream sin = sock.getInputStream();
+        
+        byte obuf [] = WifiMac.getBytes();
+        obuf[obuf.length-1] ='\0';
+        sout.write(obuf);
+        sout.flush(); // flush buffer and send to server right now
+
+        byte ibuf [] = new byte[512];
+        int len = sin.read(ibuf);
+        String is = new String(ibuf, 0, len);
+        Log.d(TAG, is);
+        if (is.contains("MAC_ERR"))
+        {
+        	ret.setTextColor(Color.RED);
+        	ret.append("\n :s-----> Server return MAC_ERR");
+            sout.close(); // socket output close
+            sin.close(); //socket input close
+            sock.close(); // socket close, both read and write
+        	return false;
+        }else if(is.contains("MAC_REJECT"))
+        {
+        	ret.setTextColor(Color.RED);
+        	ret.append("\n :s-----> Server return MAC_Reject");
+            sout.close(); // socket output close
+            sin.close(); //socket input close
+            sock.close(); // socket close, both read and write
+        	return false;        	
+        }else{
+        	ret.setTextColor(Color.GREEN);
+        	ret.append("\n :s-----> Server return MAC_OK");        	        	
+        }
+
+        sout.close(); // socket output close
+        sin.close(); //socket input close
+        sock.close(); // socket close, both read and write
+        return true;
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, 0, 0, "Quit");
+        menu.add(0, 1, 1, "About");
+        menu.add(0, 2, 2, "Network ");
+        
+        return true;
+    }
+    
+    public static boolean fileExists(String path)
+    {
+	File f = new File(path);
+	if(f.exists())
+	  return true;
+	else	
+	  return false;
+    }
+    
+    public static boolean isRk30()
+    {
+	return fileExists(RKPATH);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId())
+        {
+    		case 0:
+    			Log.d(TAG, "Logout App.");
+    			finish();
+    			return true;
+    			
+    		case 1:
+    			Log.d(TAG, "About");
+    			AlertDialog.Builder builder = new AlertDialog.Builder(MACUpActivity.this);	
+    			builder.setTitle("About")
+    					.setIcon(com.tware.macup.R.drawable.mac)
+    					.setMessage("MAC Uploader 1.5\n\n(c) Tody 2012, T-ware Inc.")
+    					.setPositiveButton("Ok", null).show();
+    			return true;
+
+    		case 2:
+    			Log.d(TAG, "Setting wireless");
+    			startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    			return true;
+
+        }
+        return false;
+    }    
+    
+    static {
+    	System.loadLibrary("readsn");
+    }
+    
+    static public native String ReadSerial();
+    static public native String rkReadSn();
+    
+    @Override
+    public void onDestroy()
+    {
+    	super.onDestroy();
+    	Process.killProcess(Process.myPid());
+    }
+
+}
